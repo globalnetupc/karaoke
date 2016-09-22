18b1eb292a168755daf69caa679485d670ae2865
Christopher Garrido <cgn_nice@msn.com>
2016-09-21 22:09:21 -0500

    CREATE SONGS AND GENERES

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :countries
   resources :cities
   resources :html
+  resources :songs
+  resources :generes
   root 'html#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
