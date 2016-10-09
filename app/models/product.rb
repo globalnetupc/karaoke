be54076fcf705dca8cad7d123aaa691f5c39d773
Christopher Garrido <cgn_nice@msn.com>
2016-10-09 19:03:32 +0000

    SEEDS Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 	self.table_name = "product"
 	def self.search(search)
-     where("product_name LIKE ?", "%#{search}%") 
-     where("product_description LIKE ?", "%#{search}%")
+     where("product_name LIKE ? OR product_description LIKE ?", "%#{search}%", "%#{search}%") 
+     #where("product_description LIKE ?", "%#{search}%")
    end
 end
